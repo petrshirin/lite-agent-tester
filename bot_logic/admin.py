26e509e4bb77ae0c692bdc8a5cb6c9f44e354ee6
Petr <p.e.shirin@gmail.com>
2020-11-28 16:48:14 +0800

    change admin panel

--- bot_logic/admin.py
+++ bot_logic/admin.py
@@ -3,10 +3,21 @@ from .models import *
 
 # Register your models here.
 
+
+@admin.register(Question)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category', 'test', 'multi_answer')
+    list_filter = ('test', 'category')
+
+
+@admin.register(Answer)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'is_right', 'question')
+    list_filter = ('question__test',)
+
+
 admin.site.register(Student)
 admin.site.register(StudentCondition)
 admin.site.register(StudentAnswer)
 admin.site.register(StudentTest)
 admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Answer)
